d9ed454a1de5e8b43a9285392e013e0b17f9a095
yhostinnapa0-cloud <yhostinnapa0@gmail.com>
2026-09-14 21:29:14 -0500

    Agregar vistas de PawCare

---
+++ Views/Mascotas/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PawCare.Views.Mascotas
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
